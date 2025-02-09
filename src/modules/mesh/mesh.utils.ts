bc0ad0472c7ca6f76f8774abacf3db060813bde4
saacostam <73088662+saacostam@users.noreply.github.com>
2025-02-09 11:22:36 -0500

    feat: option to add basic-material mesh

--- src/modules/mesh/mesh.utils.ts
+++ src/modules/mesh/mesh.utils.ts
@@ -4,20 +4,38 @@ import {
   ConeGeometry,
   CylinderGeometry,
   Mesh,
-  MeshPhongMaterial,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
   SphereGeometry,
 } from "three";
 import { COLOR_PALETTE } from "../colors";
 
+const SEGMENT_NUM = 4;
+
 export const MeshUtils = {
-  createCube: (args?: { size?: number; color?: Color }): Mesh => {
+  createCube: (args?: {
+    size?: number;
+    color?: Color;
+    basicMaterial?: boolean;
+  }): Mesh => {
     const size = args?.size ?? 1;
     const color = args?.color ?? 0x00ff00;
 
-    const geometry = new BoxGeometry(size, size, size);
-    const material = new MeshPhongMaterial({
+    const geometry = new BoxGeometry(
+      size,
+      size,
+      size,
+      SEGMENT_NUM,
+      SEGMENT_NUM,
+      SEGMENT_NUM,
+    );
+
+    const materialArgs = {
       color: color,
-    });
+    };
+    const material = args?.basicMaterial
+      ? new MeshBasicMaterial(materialArgs)
+      : new MeshStandardMaterial(materialArgs);
     return new Mesh(geometry, material);
   },
   createBox: (args?: {
@@ -25,16 +43,28 @@ export const MeshUtils = {
     height?: number;
     depth?: number;
     color?: Color;
+    basicMaterial?: boolean;
   }): Mesh => {
     const width = args?.width ?? 1;
     const height = args?.height ?? 1;
     const depth = args?.depth ?? 1;
     const color = args?.color ?? new Color(COLOR_PALETTE.RED);
 
-    const geometry = new BoxGeometry(width, height, depth);
-    const material = new MeshPhongMaterial({
+    const geometry = new BoxGeometry(
+      width,
+      height,
+      depth,
+      SEGMENT_NUM,
+      SEGMENT_NUM,
+      SEGMENT_NUM,
+    );
+
+    const materialArgs = {
       color: color,
-    });
+    };
+    const material = args?.basicMaterial
+      ? new MeshBasicMaterial(materialArgs)
+      : new MeshStandardMaterial(materialArgs);
 
     return new Mesh(geometry, material);
   },
@@ -42,26 +72,39 @@ export const MeshUtils = {
     radius?: number;
     height?: number;
     color?: Color;
+    basicMaterial?: boolean;
   }): Mesh => {
     const radius = args?.radius ?? 1;
     const height = args?.height ?? 1;
     const color = args?.color ?? new Color(COLOR_PALETTE.RED);
 
-    const geometry = new ConeGeometry(radius, height);
-    const material = new MeshPhongMaterial({
+    const geometry = new ConeGeometry(radius, height, SEGMENT_NUM, SEGMENT_NUM);
+
+    const materialArgs = {
       color: color,
-    });
+    };
+    const material = args?.basicMaterial
+      ? new MeshBasicMaterial(materialArgs)
+      : new MeshStandardMaterial(materialArgs);
 
     return new Mesh(geometry, material);
   },
-  createSphere: (args?: { radius?: number; color?: Color }): Mesh => {
+  createSphere: (args?: {
+    radius?: number;
+    color?: Color;
+    basicMaterial?: boolean;
+  }): Mesh => {
     const radius = args?.radius ?? 1;
     const color = args?.color ?? new Color(COLOR_PALETTE.RED);
 
-    const geometry = new SphereGeometry(radius);
-    const material = new MeshPhongMaterial({
+    const geometry = new SphereGeometry(radius, SEGMENT_NUM, SEGMENT_NUM);
+
+    const materialArgs = {
       color: color,
-    });
+    };
+    const material = args?.basicMaterial
+      ? new MeshBasicMaterial(materialArgs)
+      : new MeshStandardMaterial(materialArgs);
 
     return new Mesh(geometry, material);
   },
@@ -69,15 +112,26 @@ export const MeshUtils = {
     radius?: number;
     height?: number;
     color?: Color;
+    basicMaterial?: boolean;
   }): Mesh => {
     const radius = args?.radius ?? 1;
     const height = args?.height ?? 1;
     const color = args?.color ?? new Color(COLOR_PALETTE.RED);
 
-    const geometry = new CylinderGeometry(radius, radius, height);
-    const material = new MeshPhongMaterial({
+    const geometry = new CylinderGeometry(
+      radius,
+      radius,
+      height,
+      SEGMENT_NUM,
+      SEGMENT_NUM,
+    );
+
+    const materialArgs = {
       color: color,
-    });
+    };
+    const material = args?.basicMaterial
+      ? new MeshBasicMaterial(materialArgs)
+      : new MeshStandardMaterial(materialArgs);
 
     return new Mesh(geometry, material);
   },
