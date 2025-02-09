476a990401298b173db1b2071ec7c4ed317ba1d7
Santiago Acosta <73088662+saacostam@users.noreply.github.com>
2025-02-08 19:05:32 -0500

    feat: add three js to game loop (#1)
    * feat: pass scene as part of update and graphics methods
    
    * feat: add three-js logic to existing game loop
    
    * feat: implement kill logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 import "./style.css";
+
+import { Actor, Container, Game } from "./modules/game";
+import { MeshUtils } from "./modules/mesh";
+import { Vector2 } from "three";
+
+const game = new Game();
+
+const testContainer = new Container({ width: 8, height: 8 });
+
+const testActor = new Actor({
+  mesh: MeshUtils.createCube(),
+});
+
+testActor.update = () => {
+  if (Math.random() < 0.001) testActor.kill();
+};
+
+testActor.graphics = (_, delta) => {
+  testActor.mesh.rotation.x += delta / 1000;
+  testActor.mesh.rotation.y += delta / 1000;
+};
+
+testContainer.addActor(testActor, new Vector2(0, 0));
+
+game.addContainer("test", testContainer);
+game.setContainer("test");
+game.start();
--- src/modules/game/actor/actor.ts
+++ src/modules/game/actor/actor.ts
@@ -1,7 +1,32 @@
+import { Mesh, Vector2 } from "three";
+import { Container } from "../container";
 import { Game } from "../game";
 
+export interface ActorArgs {
+  mesh: Mesh;
+}
+
 export class Actor {
-  public constructor() {}
-  public update(_game: Game, _delta: number) {}
-  public graphics(_game: Game, _delta: number) {}
+  public mesh: Mesh;
+  public isAlive = true;
+
+  public constructor(args: ActorArgs) {
+    this.mesh = args.mesh;
+  }
+
+  public update(
+    _game: Game,
+    _delta: number,
+    _container: Container,
+    _pos: Vector2,
+  ) {}
+  public graphics(
+    _game: Game,
+    _delta: number,
+    _container: Container,
+    _pos: Vector2,
+  ) {}
+  public kill() {
+    this.isAlive = false;
+  }
 }
