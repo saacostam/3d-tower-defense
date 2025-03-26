44680d6c61721c5e313e965007f70f42f00dfaad
saacostam <73088662+saacostam@users.noreply.github.com>
2025-03-26 01:18:57 -0500

    refactor: rename tree to match container type

--- src/modules/battlefield-container/components/index.ts
+++ src/modules/battlefield-container/components/index.ts
@@ -2,5 +2,5 @@ export * from "./bridge.component";
 export * from "./grass.component";
 export * from "./mountain.component";
 export * from "./star.component";
-export * from "./tree.actor";
+export * from "./tree.component";
 export * from "./water.component";
